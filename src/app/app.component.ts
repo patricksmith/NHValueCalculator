a76b14cf1bd03f0887118b4e9fb89766b46206a3
Jeff Deville <jeffdeville@gmail.com>
2017-10-24 17:26:45 -0400

    Create some demo data for the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,86 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  single: any[] = [
+    {
+      'name': 'Germany',
+      'value': 8940000
+    },
+    {
+      'name': 'USA',
+      'value': 5000000
+    },
+    {
+      'name': 'France',
+      'value': 7200000
+    }
+  ];
+  multi: any[] = [
+    {
+      'name': 'Germany',
+      'series': [
+        {
+          'name': '2010',
+          'value': 7300000
+        },
+        {
+          'name': '2011',
+          'value': 8940000
+        }
+      ]
+    },
+    {
+      'name': 'USA',
+      'series': [
+        {
+          'name': '2010',
+          'value': 7870000
+        },
+        {
+          'name': '2011',
+          'value': 8270000
+        }
+      ]
+    },
+    {
+      'name': 'France',
+      'series': [
+        {
+          'name': '2010',
+          'value': 5000002
+        },
+        {
+          'name': '2011',
+          'value': 5800000
+        }
+      ]
+    }
+  ];
+
+  view: any[] = [700, 400];
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+
+  // constructor() {
+  //   Object.assign(this, {single, multi});
+  // }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
 }
